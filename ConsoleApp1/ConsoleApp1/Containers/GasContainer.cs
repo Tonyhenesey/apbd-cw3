846ed0a16349bcf615046a28d21331887c7e7925
s27928 <s27928@pjwstk.edu.pl>
2024-03-24 21:02:00 +0100

    GasContainer commit

---
+++ ConsoleApp1/ConsoleApp1/Containers/GasContainer.cs
@@ -0,0 +1,30 @@
+using ConsoleApp1.Interfaces;
+
+namespace ConsoleApp1.Containers;
+
+public class GasContainer : Container , IHazardNotifer
+{
+    private double pressure;
+    private GasContainer _gasContainer;
+    private LoadType _loadType;
+    private int _serialNumber;
+    
+    
+
+    public GasContainer(int serialNumber,double pressure, double height, double selfWeight, double depth, double cargoWeight, LoadType loadType) : base(serialNumber, height, selfWeight, depth, cargoWeight, loadType)
+    {
+        _loadType = loadType;
+        _serialNumber = serialNumber;
+
+    }
+    public void SendHazardNotification(string message)
+    {
+        if (_loadType==LoadType.DANGEROUS)
+        {
+            Console.WriteLine("Attention! \n" +
+                              "DANGEROUS LOAD DETECTED\n" +
+                              "POSIBILITY OF EXPLODING!\n" +
+                              "CONTAINER: "+ _serialNumber);
+        }
+    }
+}
