ffb2b2751c88044cbb56ada83239c6d3b4d9c8aa
s27928 <s27928@pjwstk.edu.pl>
2024-03-24 22:33:07 +0100

    Main Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,43 +1,55 @@
-﻿using ConsoleApp1;
-using ConsoleApp1.Containers;
+﻿using ConsoleApp1.Containers;
 using ConsoleApp1.ContainerShip;
-//
-// // var a = 1;
-// // var b = "text";
-// // var c = true;
-// //
-// // var container = new Container()
-// {
-//     // CargoWeight = 10.0
-// };
-//
-// // container.Unload(5.0);
-// // container.Load(10);
-// // Console.WriteLine(container.CargoWeight);
-//
-// var lc = new LiquidContainer(13, 2, 3, 4, 2,Container.LoadType.DANGEROUS);
-// var lc2 = new LiquidContainer(14, 2, 3, 4, 1,Container.LoadType.NORMAL);
-// var lc3 = new LiquidContainer(14, 2, 3, 4, 1,Container.LoadType.NORMAL);
-//
-// // int? number = 1;
-// // number = null;
-// //
-// // Exception? ex = new Exception();
-// // ex=null;
-// //
-// // if ( ex != null) Console.WriteLine(ex.Message);
-// //
-// // Container container = new Container(10.0);
-// // Container liquiCon = new LiquidContainer(10.2);
-//
-// // ArrayList
-// List<int> list = new List<int>() { 1, 2, 3, 4 };
-// List<int> list2 = new () { 1, 2, 3, 4 };
-//
-//
-// // HashMap
-// Container container = new Container();1
-List<Container> containers = null;
-ContainerShip containerShip =
-    new ContainerShip(1, 50, 50, 50, 50, Container.LoadType.DANGEROUS, containers, 100, 200, 200);
-containerShip.CreateContainer();
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Container> containers = new List<Container>();
+        ContainerShip containerShip = new ContainerShip(containers, 20, 10000, 100);
+
+        while (true)
+        {
+            Console.WriteLine("Wybierz operację:\n" +
+                              "1. Dodaj kontenerowiec\n" +
+                              "2. Załaduj kontener na statek\n" +
+                              "3. Usuń kontener ze statku\n" +
+                              "4. Wyświetl informacje o statku\n" +
+                              "5. Wyjście");
+            
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "1":
+                    // ContainerShip containerShip2 = new ContainerShip();
+                    break;
+
+                case "2":
+                    containerShip.CreateContainer();
+                    if(containerShip != null)
+                    {
+                        containerShip.LoadContainer(containerShip);
+                    }
+                    break;
+
+                case "3":
+                    containerShip.UnloadContainer(containerShip);
+                    break;
+
+                case "4":
+                    containerShip.PrintShipInfo();
+                    break;
+
+                case "5":
+                    return;
+
+                default:
+                    Console.WriteLine("Nieprawidłowa operacja.");
+                    break;
+            }
+        }
+    }
+}
