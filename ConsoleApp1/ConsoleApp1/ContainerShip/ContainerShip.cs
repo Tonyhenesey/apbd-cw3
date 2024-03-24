74bbe969fdb5efb6884674aa0e143b2e5a43b418
s27928 <s27928@pjwstk.edu.pl>
2024-03-24 21:53:03 +0100

    ContainerShip Update

--- ConsoleApp1/ConsoleApp1/ContainerShip/ContainerShip.cs
+++ ConsoleApp1/ConsoleApp1/ContainerShip/ContainerShip.cs
@@ -8,9 +8,24 @@ public class ContainerShip : Container
     private double _maxvelocity;
     private double _maxShipweight;
     private int _maxContainerCount;
-    
+    int _serialNumber;
+    double _pressure;
+    double _height;
+    double _selfWeight;
+    double _depth;
+    double _cargoWeight;
+    LoadType _loadType;
+    private double _temperature;
 
-    public ContainerShip(List<Container> containers, double maxvelocity, double maxShipweight, int maxContainerCount) : base()
+    public ContainerShip(List<Container> containers, double maxvelocity, double maxShipweight, int maxContainerCount)
+    {
+        _containers = containers;
+        _maxvelocity = maxvelocity;
+        _maxShipweight = maxShipweight;
+        _maxContainerCount = maxContainerCount;
+    }
+
+    public ContainerShip(int serialNumber, double height, double selfWeight, double depth, double cargoWeight, LoadType loadType, List<Container> containers, double maxvelocity, double maxShipweight, int maxContainerCount) : base(serialNumber, height, selfWeight, depth, cargoWeight, loadType)
     {
         _containers = containers;
         _maxvelocity = maxvelocity;
@@ -28,42 +43,88 @@ public class ContainerShip : Container
 
         string userInput = Console.ReadLine();
 
+        
         switch (userInput)
         {
             case "1":
                 Console.WriteLine("Podaj numer seryjny:");
-                int serialNumber = int.Parse(Console.ReadLine());
+                _serialNumber = int.Parse(Console.ReadLine());
 
                 Console.WriteLine("Podaj ciśnienie:");
-                double pressure = double.Parse(Console.ReadLine());
+               _pressure = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Podaj wysokość:");
-                double height = double.Parse(Console.ReadLine());
+                _height = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Podaj własną masę:");
-                double selfWeight = double.Parse(Console.ReadLine());
+                _selfWeight = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Podaj głębokość:");
-                double depth = double.Parse(Console.ReadLine());
+                _depth = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Podaj masę ładunku:");
-                double cargoWeight = double.Parse(Console.ReadLine());
+                _cargoWeight= double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Podaj typ ładunku (0 - DANGEROUS, 1 - NORMAL):");
-                LoadType loadType = (LoadType)int.Parse(Console.ReadLine());
+                _loadType = (LoadType)int.Parse(Console.ReadLine());
 
-                container = new GasContainer(serialNumber, pressure, height, selfWeight, depth, cargoWeight, loadType);
+                container = new GasContainer(_serialNumber,_pressure,_height,_selfWeight,_depth,_cargoWeight,_loadType);
                 break;
 
             case "2":
-                container = new LiquidContainer();
+                Console.WriteLine("Podaj numer seryjny:");
+                _serialNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj wysokość:");
+                _height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj własną masę:");
+                _selfWeight = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj głębokość:");
+                _depth = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj masę ładunku:");
+                _cargoWeight = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj typ ładunku (0 - DANGEROUS, 1 - NORMAL):");
+                _loadType = (LoadType)int.Parse(Console.ReadLine());
+
+                container = new LiquidContainer(_serialNumber, _height, _selfWeight, _depth, _cargoWeight, _loadType);
                 break;
+
+
             case "3":
-                container = new CoolerContainer();
+                Console.WriteLine("Podaj numer seryjny:");
+                _serialNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj wysokość:");
+                _height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj własną masę:");
+                _selfWeight = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj głębokość:");
+                _depth = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj masę ładunku:");
+                _cargoWeight = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj typ ładunku (0 - DANGEROUS, 1 - NORMAL):");
+                _loadType = (LoadType)int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Wybierz możliwe produkty (podaj liczby odzielone przecinkami):\n0. Banana\n1. Chocolate\n2. Fish\n3. Meat\n4. Ice_Cream\n5. Frozen_Pizza\n6. Cheese\n7. Sausages\n8. Butter\n9. Eggs");
+                PossibleProducts _possibleProducts = (PossibleProducts)int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj temperaturę:");
+                _temperature = double.Parse(Console.ReadLine());
+
+                container = new CoolerContainer(_serialNumber, _height, _selfWeight, _depth, _cargoWeight, _loadType, _possibleProducts, _temperature);
                 break;
+
             default:
                 Console.WriteLine("Niepoprawny wybór. Proszę wybrać numer od 1 do 3.");
-                return; // Wyjście z metody, jeśli wybór jest niepoprawny
+                return; 
         }
 
         Console.WriteLine($"Utworzono kontener typu: {container.GetType().Name}");
